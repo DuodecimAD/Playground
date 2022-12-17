732212b5687d8439c20b3c2001326bc685bd4677
Duodecim-Main <Duodecim-Main@Duo-Main.fritz.box>
2022-12-17 11:44:30 +0100

    first commit

---
+++ src/Sandbox.java
@@ -0,0 +1,19 @@
+
+public class Sandbox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 0, a = 0;
+		
+		for(i=0; i<10; i++) {
+			a = i*2;
+			System.out.print(a+ " ");
+		}
+		System.out.println();
+		for(i=0; i<10; i++) {
+			System.out.print(a+ " ");
+		}
+	}
+
+}
